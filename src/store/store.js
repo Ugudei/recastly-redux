c3e31793a8f8cfa8d58ca34d67e4de07a1eda2c3
Ugudei <ugudeit@gmail.com>
2020-01-02 17:37:39 -0800

    passes test refactor pending

--- src/store/store.js
+++ src/store/store.js
@@ -3,4 +3,12 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
+
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+// console.log(exampleVideoData);
+export default createStore(
+  rootReducer,
+  { currentVideo: null,
+    videoList: exampleVideoData }, // state is always an object,
+  applyMiddleware(thunk)
+);
